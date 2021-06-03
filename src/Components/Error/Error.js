7be48eda97ddcd8bdd01f09dd086eedb69d19815
Prathmesh Khandelwal <201952225@iiitvadodara.ac.in>
2021-06-03 20:45:23 +0530

    error handling done

--- src/Components/Error/Error.js
+++ src/Components/Error/Error.js
@@ -12,9 +12,11 @@ const Error = (props) => {
   console.log(props.err);
   const res = showModal ? (
     <div onClick={handleModal} className="Error" id="error">
+      
       <div className="close">
         <p>x</p>
       </div>
+      <h1 style={{ margin: "0", padding: "10px 0", color:'red', textAlign:'center' }}>OOPS! There is an Error</h1>
       <div className="textarea">{props.err}</div>
     </div>
   ) : null;
